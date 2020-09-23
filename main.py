b1479456f9b53657a6862b15885181a18f5b101d
digiprospector <digiprospector@protonmail.com>
2020-09-23 01:54:55 -0400

    1. add progress bar. 2.add retry if download file fail

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 from pyquery import PyQuery as pq
 import time
 import threading
+import tqdm
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.133 Safari/537.39'
 DL_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.{:04d}.{:03d} Safari/537.39'
@@ -24,6 +25,10 @@ DOWNLOAD_DIR = 'download'
 
 DL_ERROR_FILELINKTIMEOUT = '下载链接已超时，请重新从文件夹获取。'
 DL_Threads_cnt = 16
+
+def requests_debug(r, prefix=''):
+    log.debug('{}requests:{}'.format(prefix, r.request.url))
+    log.debug('{}response:{}'.format(prefix, r.text))
 
 class CtFile():
     def __init__(self,
@@ -52,11 +57,12 @@ class CtFile():
         }
         r = self.s.get(GET_FILE_URL1, params=params, headers=headers)
         j = json.loads(r.text)
-        log.debug('step 1 r={}'.format(json.dumps(j)))
+        log.debug('step 1')
+        requests_debug(r)
 
         # link error handler
         if j.get('code') == 404:
-            log.error('dl_file error: {}, {}'.format(url, j.get('message')))
+            log.error('dl_file error: {}'.format(j.get('message')))
             if j.get('message') == DL_ERROR_FILELINKTIMEOUT:
                 log.error('need get dir list again')
             return False, j.get('message')
@@ -75,9 +81,15 @@ class CtFile():
             'verifycode': '',
             'rd': str(random.random())
         }
-        r = self.s.get(GET_FILE_URL2, params=params, headers=headers)
-        j = json.loads(r.text)
-        log.debug('step 2 r={}'.format(json.dumps(j)))
+        while True:
+            r = self.s.get(GET_FILE_URL2, params=params, headers=headers)
+            j = json.loads(r.text)
+            log.debug('step 2')
+            requests_debug(r)
+            if j.get('code') == 503:
+                params['rd'] = str(random.random())
+            else:
+                break
 
         # create an empty file
         filename = os.path.join(DOWNLOAD_DIR, self.folder, fn)
@@ -99,12 +111,25 @@ class CtFile():
                           filename,
                           start,
                           end)
-            log.info('dl-{:03d} start={} end={}'.format(i+1, start, end))
+            log.debug('dl-{:03d} download range start={} end={}'.format(i+1, start, end))
 
             threads.append(t)
             t.start()
+            #time.sleep(1)
+
+        progressbar = tqdm.tqdm(total=filesize, desc=filename, ascii=' #', unit="B", unit_scale=True, unit_divisor=1024)
+        downloaded_bytes = 0
+        last_downloaded_bytes = 0
+        while downloaded_bytes < filesize:
+            downloaded_bytes = 0
+            for t in threads:
+                downloaded_bytes += t.downloaded_bytes()
+            progressbar.update(downloaded_bytes-last_downloaded_bytes)
+            last_downloaded_bytes = downloaded_bytes
+            log.debug("{} {}".format(downloaded_bytes, filesize))
             time.sleep(1)
 
+        log.debug('quit')
         for i in range(DL_Threads_cnt):
             threads[i].join()
 
@@ -122,42 +147,46 @@ class DL_Thread(threading.Thread):
         self._end = end
         self._UA = DL_UA.format(random.randrange(9999), self._index + 1)
         self._s = requests.session()
+        self._downloaded_bytes = 0
 
         self._headers['user-agent'] = self._UA
 
     def run(self):
         # step 2
         while True:
-            self._params['rd'] = str(random.random())
-            r = self._s.get(GET_FILE_URL2, params=self._params, headers=self._headers)
-            j = json.loads(r.text)
-            log.info(r.status_code)
-            log.info('dl-{:03d} step 2 r={}'.format(self._index + 1, json.dumps(j)))
-            if j['code'] == 503 and j['message'] == 'require for verifycode':
-                log.info('dl-{:03d} retry'.format(self._index + 1))
-                self._UA = DL_UA.format(random.randrange(9999), self._index + 1)
-                self._headers['user-agent'] = self._UA
-            else:
-                break
+            while True:
+                self._params['rd'] = str(random.random())
+                r = self._s.get(GET_FILE_URL2, params=self._params, headers=self._headers)
+                j = json.loads(r.text)
+                log.debug('dl-{:03d} step 2'.format(self._index + 1))
+                requests_debug(r, 'dl-{:03d} '.format(self._index + 1))
+                if j['code'] == 503 and j['message'] == 'require for verifycode':
+                    log.debug('dl-{:03d} retry'.format(self._index + 1))
+                    self._UA = DL_UA.format(random.randrange(9999), self._index + 1)
+                    self._headers['user-agent'] = self._UA
+                else:
+                    break
 
-        with open(self._filename, 'r+b') as f:
-            f.seek(self._start)
-            log.info(j)
-            self._headers['Range'] = 'bytes={}-{}'.format(self._start, self._end)
-            r = self._s.get(j['downurl'].replace(r'\/', r'/'), headers=self._headers, stream=True)
-            if r.status_code == 503:
-                log.info(r.request.headers)
-                log.info(r.text)
-            s = 0
-            t = time.time()
-            l = t
-            for chunk in r.iter_content(chunk_size=128):
-                f.write(chunk)
-                s += len(chunk)
-                n = time.time()
-                if n - l > 3:
-                    log.info('dl-{:03d} speed {:02f} KB/s'.format(self._index + 1, s/(n-t)/1024))
-                    l = n
+            with open(self._filename, 'r+b') as f:
+                f.seek(self._start)
+                self._headers['Range'] = 'bytes={}-{}'.format(self._start, self._end)
+                r = self._s.get(j['downurl'].replace(r'\/', r'/'), headers=self._headers, stream=True)
+                log.debug('dl-{:03d} download file request: {}'.format(self._index+1, r.status_code))
+                if r.status_code == 503:
+                    log.warning('dl-{:03d} download fail, retry'.format(self._index + 1))
+                    self._headers['user-agent'] = DL_UA.format(random.randrange(9999), self._index + 1)
+                    time.sleep(1)
+                else:
+                    for chunk in r.iter_content(chunk_size=128):
+                        f.write(chunk)
+                        self._downloaded_bytes += len(chunk)
+                    log.debug('dl-{:03d} exit'.format(self._index + 1))
+                    break
+
+
+
+    def downloaded_bytes(self):
+        return self._downloaded_bytes
 
 class CtDir():
     def __init__(self, args):
@@ -216,7 +245,7 @@ class CtDir():
             }
             r = self.s.get(GET_DIR_URL, params=params, headers=headers)
             j = json.loads(r.text.encode().decode('utf-8-sig'))
-            log.debug('r={}'.format(json.dumps(j)))
+            requests_debug(r)
             if j.get('code') == 404 or j.get('code') == 503:
                 log.error('dl_dir_list error: {}, {}'.format(self.url, j.get('message')))
 
@@ -258,81 +287,6 @@ class CtDir():
 
         return True, None
 
-    '''
-    def dl_file(self, url, fn):
-        loc = self.status['loc']
-        log.info('download {} {}'.format(fn, url))
-
-        # step 1
-        headers = {
-            #'user-agent': MOBILE_UA,
-            'origin': self.urlparsed.netloc,
-        }
-        # parameters
-        params = {
-            'f' : url.split('/')[-1],
-            'passcode' : '',
-            'r' : str(random.random()),
-            'ref' : '',
-        }
-        r = self.s.get(GET_FILE_URL1, params=params, headers=headers)
-        j = json.loads(r.text)
-        log.debug('step 1 r={}'.format(json.dumps(j)))
-
-        # link error handler
-        if j.get('code') == 404:
-            log.error('dl_file error: {}, {}'.format(url, j.get('message')))
-            if j.get('message') == DL_ERROR_FILELINKTIMEOUT:
-                log.error('need get dir list again')
-            return False, j.get('message')
-
-        # step 2
-        params = {
-            'uid': j['userid'],
-            'fid': j['file_id'],
-            'folder_id': 0,
-            'file_chk': j['file_chk'],
-            'mb': 0,
-            'app': 0,
-            'acheck': 1,
-            'verifycode': '',
-            'rd': str(random.random())
-        }
-        r = self.s.get(GET_FILE_URL2, params=params, headers=headers)
-        j = json.loads(r.text)
-        log.debug('step 2 r={}'.format(json.dumps(j)))
-
-        # create an empty file
-        filename = os.path.join(DOWNLOAD_DIR, loc['name'], fn)
-        filesize = int(j['file_size'])
-        log.debug('create empty file {} size {}'.format(filename, filesize))
-        with open(filename, 'wb') as fd:
-            fd.truncate(filesize)
-
-        #donwload with thread
-        threads = []
-        for i in range(DL_Threads_cnt):
-            start = i * filesize // DL_Threads_cnt
-            end = (i + 1) * filesize // DL_Threads_cnt - 1 if i != DL_Threads_cnt - 1 else filesize
-
-            t = DL_Thread(i,
-                          j['downurl'].replace(r'\/', r'/'),
-                          params,
-                          headers,
-                          filename,
-                          start,
-                          end)
-            log.info('dl-{:03d} start={} end={}'.format(i+1, start, end))
-
-            threads.append(t)
-            t.start()
-            time.sleep(1)
-
-        for i in range(DL_Threads_cnt):
-            threads[i].join()
-
-        return True, None
-    '''
 def main():
     parser = argparse.ArgumentParser(description='Download from CTDisk.')
     parser.add_argument('-d', '--dir', help='download a directory')
@@ -357,6 +311,7 @@ def main():
     elif args.file:
         ct_file = CtFile(url=args.file)
         ct_file.dl()
+        log.info('download finished')
 
 if __name__ == "__main__":
     main()
